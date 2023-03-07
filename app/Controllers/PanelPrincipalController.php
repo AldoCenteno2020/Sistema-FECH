04f211ec0418e86e85d534365118b79735739994
Aldo <aldo14az@gmail.com>
2023-03-07 10:11:44 -0500

    nuevo commit

---
+++ app/Controllers/PanelPrincipalController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class PanelPrincipalController extends BaseController
+{
+    public function PanelView()
+    {
+        return view('PanelPrincipal/PanelPrincipalView');
+    }
+}
